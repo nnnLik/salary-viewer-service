d42e1c7552476fc35878abc1eae7d38f0fcc9ae4
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-26 01:09:15 +0300

    [ADD] auth schemas

---
+++ src/schemas/auth.py
@@ -0,0 +1,36 @@
+from typing import Optional
+import uuid
+
+from pydantic import EmailStr
+
+from fastapi_users import schemas, models
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    id: models.ID
+    first_name: str
+    last_name: str
+    birth_year: int
+    email: EmailStr
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+
+class UserCreate(schemas.BaseUserCreate):
+    first_name: str
+    last_name: str
+    birth_year: int
+    email: EmailStr
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    password: Optional[str]
+    email: Optional[EmailStr]
+    is_active: Optional[bool]
+    is_superuser: Optional[bool]
+    is_verified: Optional[bool]
