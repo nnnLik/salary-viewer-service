3af7701db4b0747d072fb894cd9bfa70a05adb62
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-27 17:20:23 +0300

    [ADD] test db connection

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pydantic import BaseSettings
+
 from dotenv import dotenv_values
 
 
@@ -6,6 +7,14 @@ class ServerSettings(BaseSettings):
     HOST: str
     PORT: int
     SECRET: str
+
+
+class TestDatabaseSettings(BaseSettings):
+    DB_HOST_TEST: str
+    DB_PORT_TEST: int
+    DB_NAME_TEST: str
+    DB_USER_TEST: str
+    DB_PASS_TEST: str
 
 
 class DatabaseSettings(BaseSettings):
@@ -19,6 +28,7 @@ class DatabaseSettings(BaseSettings):
 class Settings(BaseSettings):
     server: ServerSettings = ServerSettings()
     database: DatabaseSettings = DatabaseSettings()
+    test_database: TestDatabaseSettings = TestDatabaseSettings()
 
 
 def load_settings() -> Settings:
@@ -36,6 +46,12 @@ def load_settings() -> Settings:
     settings.database.DB_NAME = env_values.get("DB_NAME")
     settings.database.DB_USER = env_values.get("DB_USER")
     settings.database.DB_PASS = env_values.get("DB_PASS")
+
+    settings.test_database.DB_HOST_TEST = env_values.get("DB_HOST_TEST")
+    settings.test_database.DB_PORT_TEST = int(env_values.get("DB_PORT_TEST"))
+    settings.test_database.DB_NAME_TEST = env_values.get("DB_NAME_TEST")
+    settings.test_database.DB_USER_TEST = env_values.get("DB_USER_TEST")
+    settings.test_database.DB_USER_TEST = env_values.get("DB_PASS_TEST")
 
     return settings
 
